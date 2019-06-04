3d16ae8d03c9bbadfeda34ce47b18bc81e44225c
Felipe Lobo <jflobom@gmail.com>
2019-06-04 18:58:19 -0300

    Add tweets CRUD

---
+++ app/Controllers/Http/TweetController.js
@@ -0,0 +1,74 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Tweet = use( 'App/Models/Tweet' )
+
+/**
+ * Resourceful controller for interacting with tweets
+ */
+class TweetController {
+  /**
+   * Show a list of all tweets.
+   * GET tweets
+   *
+   */
+  async index() {
+    const tweets = await Tweet.query().with('user').fetch();
+
+    return tweets;
+  }
+
+  /**
+   * Create/save a new tweet.
+   * POST tweets
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, auth }) {
+    const data = request.only( ['content'] );
+    const tweet = await Tweet.create( { user_id: auth.user.id, ...data } );
+
+    return tweet;
+  }
+
+  /**
+   * Display a single tweet.
+   * GET tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {
+    const tweet = await Tweet.findOrFail( params.id );
+
+    return tweet;
+  }
+
+
+  /**
+   * Delete a tweet with id.
+   * DELETE tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, auth, response }) {
+    const tweet = await Tweet.findOrFail( params.id );
+
+    if ( tweet.user_id !== auth.user.id ) {
+      response.status(401)
+    }
+
+      await tweet.delete()
+  }
+}
+
+module.exports = TweetController
--- start/routes.js
+++ start/routes.js
@@ -18,3 +18,7 @@ const Route = use('Route')
 
 Route.post('/register', 'AuthController.register');
 Route.post('/authenticate', 'AuthController.authenticate');
+
+Route.group(() => {
+  Route.resource('tweets', 'TweetController').apiOnly().except('update')
+}).middleware('auth')
